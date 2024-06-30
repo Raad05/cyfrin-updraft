dcd587df4c564df8792e13f461b2fb96a1cd76e7
Raad05 <ashabulraad@gmail.com>
2024-06-30 13:19:45 +0600

    import update

--- Smart Contract Development/Storage Factory/StorageFactory.sol
+++ Smart Contract Development/Storage Factory/StorageFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./SimpleStorage.sol";
+import "../Simple Storage/SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage public simpleStorage;
