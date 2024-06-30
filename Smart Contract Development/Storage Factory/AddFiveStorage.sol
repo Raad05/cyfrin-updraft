25cfa719f0054ce951d88d4675c3ad2dcea5f490
Raad05 <ashabulraad@gmail.com>
2024-06-30 14:17:39 +0600

    virtual functions and overriding in inheritence

--- Smart Contract Development/Storage Factory/AddFiveStorage.sol
+++ Smart Contract Development/Storage Factory/AddFiveStorage.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.9;
 
 import {SimpleStorage} from "../Simple Storage/SimpleStorage.sol";
 
-contract AddFiveStorage is SimpleStorage {}
+contract AddFiveStorage is SimpleStorage {
+    function store(uint256 _favoriteNumber) public override {
+        myFavoriteNumber = _favoriteNumber + 5;
+    }
+}
