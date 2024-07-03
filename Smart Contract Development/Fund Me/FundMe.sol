66725909360938e9e1ca7fac7b2486201b671d26
Raad05 <ashabulraad@gmail.com>
2024-07-03 14:13:44 +0600

    getVersion function

--- Smart Contract Development/Fund Me/FundMe.sol
+++ Smart Contract Development/Fund Me/FundMe.sol
@@ -5,6 +5,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     function fund() public payable {
         require(msg.value > 1e18, "Not enough ETH sent.");
@@ -15,4 +17,10 @@ contract FundMe {
     function getPrice() public {}
 
     function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256) {
+        return
+            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
+                .version();
+    }
 }
