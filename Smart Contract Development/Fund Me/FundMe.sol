8c118c88340f6d23a722e66faca8952eaf6fe2d6
Raad05 <ashabulraad@gmail.com>
2024-07-04 22:09:36 +0600

    funders reset

--- Smart Contract Development/Fund Me/FundMe.sol
+++ Smart Contract Development/Fund Me/FundMe.sol
@@ -25,5 +25,11 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public {
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
 }
