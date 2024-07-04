5f9b7bbeca27ec2fc36aa229e85435dcf02ebc80
Raad05 <ashabulraad@gmail.com>
2024-07-04 22:02:48 +0600

    fund function refactored

--- Smart Contract Development/Fund Me/FundMe.sol
+++ Smart Contract Development/Fund Me/FundMe.sol
@@ -12,11 +12,17 @@ contract FundMe {
     using PriceConverter for uint256;
     uint256 public minimumUSD = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
+
     function fund() public payable {
         require(
             msg.value.getConversionRate() > minimumUSD,
             "Not enough ETH sent."
         );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() public {}
