bf77584730c6415cefa56abc0802657f516c31c7
Raad05 <ashabulraad@gmail.com>
2024-07-04 20:56:46 +0600

    FundMe contract refactored

--- Smart Contract Development/Fund Me/FundMe.sol
+++ Smart Contract Development/Fund Me/FundMe.sol
@@ -6,38 +6,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     uint256 public minimumUSD = 5e18;
-    AggregatorV3Interface public feed =
-        AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
     function fund() public payable {
         require(
-            getConversionRate(msg.value) > minimumUSD,
+            msg.value.getConversionRate() > minimumUSD,
             "Not enough ETH sent."
         );
     }
 
     function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-        (, int256 answer, , , ) = feed.latestRoundData();
-
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(
-        uint256 _ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 convertedRate = (ethPrice * _ethAmount) / 1e18;
-
-        return convertedRate;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return feed.version();
-    }
 }
